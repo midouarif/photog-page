6f217fe862d17a663927818a29850a767a3f7e46
midouarif <midouarif4@gmail.com>
2024-03-25 12:07:37 +0100

    Add dotenv and mongoose dependencies, connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log(error);
+});
 
 const app = express();
 app.listen(3000, () => {
