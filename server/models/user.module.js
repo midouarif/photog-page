944dbbc47da42ed59f6e97e3d4e9daae3710b6dc
midouarif <midouarif4@gmail.com>
2024-03-25 14:11:34 +0100

    Add user module to server

---
+++ server/models/user.module.js
@@ -0,0 +1,21 @@
+import { timeStamp } from 'console';
+import mongoose from 'mongoose';
+const userShema = new mongoose.Schema({
+    username:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+},{timeStamp: true});
+const User = mongoose.model('User',userShema);
+export default User;
