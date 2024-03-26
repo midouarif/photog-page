b10268750e774a4ec995aeb9ebdc78e64d8e5923
midouarif <midouarif4@gmail.com>
2024-03-27 00:13:58 +0100

    Add JWT authentication and signin functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/server/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success===false) {
+        setError(true);
+        return;
+      }
+
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' />
-        <input type='text' placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
-        <input type='password' placeholder='password' id='password' className='bg-slate-100 p-3 rounded-lg' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <input type='text' placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <input type='password' placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <button type='submit' disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ?"loading":"Sign up"}</button>
       </form>
       <div className='text-center flex gap-2 mt-5'>
         <p className=''>Already have an account?</p>
         <Link to='/signin'><span className=' font-semibold text-blue-500'>Sign in</span></Link>
       </div>
-
+      <p className='text-center text-red-500 mt-3 font-semibold'>{error && 'An error occurred'}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.module.js";
 import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res,next) => {
@@ -12,7 +13,27 @@ export const signup = async (req, res,next) => {
    } catch (error) {
     next(error);
    }
-
+}
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   try {
+      const validUser = await User.findOne({email});
+      if (!validUser) {
+         return res.status(404).json({message: "User not found!"});
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+         return res.status(401).json({message: "Invalid password!"});
+      }
+      // generate token
+      const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      // to exclude password from the response
+      const {password: hashedPassword, ...rest} = validUser._doc;
+      // send token as cookie
+      res.cookie('token', token, {httpOnly: true}).status(200).json({rest});
+   } catch (error) {
+      next(error);
+   }
 }
 export default signup;
 
